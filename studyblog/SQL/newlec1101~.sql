7b77dfc9d0d6ba1dbb831d66155440db0adf521a
jiheejiheekim <jihee2518@gmail.com>
2023-11-14 19:21:50 +0900

    11/14 19시

--- studyblog/SQL/newlec1101~.sql
+++ studyblog/SQL/newlec1101~.sql
@@ -43,5 +43,57 @@ insert into notice values(21, '수료 50% 남음', 'fighting', '하지만 다 해야돼', s
 ------------------------------------------------------------------------11/14
 select * from notice;
 
+--getNoticeList
+SELECT ROWNUM, NOTICE.* FROM NOTICE ORDER BY REGDATE DESC;
+--역순으로 ROWNUM됨 사용불가
+
+
+--방법 1
+SELECT * FROM (
+    SELECT ROWNUM NUM, N.* 
+    FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N
+)
+WHERE NUM BETWEEN 6 AND 10;
+--먼저 날짜 내림 차순으로 정렬 후 N으로 별칭을 붙임, 그 다음 ROWNUM 사용가능
+
+--방법 2
+SELECT * FROM (
+    SELECT ROW_NUMBER() OVER (ORDER BY REGDATE DESC) NUM,
+    NOTICE.* FROM NOTICE
+    )
+WHERE NUM BETWEEN 6 AND 10;
+
+
+
+
+
+
+
+--getNoticeCount
+
+
+
+--getNextNotice
+
+SELECT * FROM NOTICE
+WHERE ID= (
+    SELECT ID FROM NOTICE WHERE 
+);
+
+SELECT ID FROM NOTICE
+WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID=3);
+--작성일이 아이디3번의 작성일보다 큰 게시글의 아이디를 구하는 문장
+
+--위 문장에서 4번만 나옴
+SELECT ID FROM NOTICE
+WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID=3)
+AND ROWNUM=1;
+
+--getPrevNotice
+SELECT ID FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) 
+WHERE REGDATE < (SELECT REGDATE FROM NOTICE WHERE ID=3)
+AND ROWNUM=1;
+
+
 
     
