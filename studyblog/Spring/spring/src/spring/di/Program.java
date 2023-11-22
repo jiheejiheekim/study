aa3070621eefc153158511819a29ddd36a420d09
jiheejiheekim <jihee2518@gmail.com>
2023-11-22 20:38:55 +0900

    11/22 20시 39분

---
+++ studyblog/Spring/spring/src/spring/di/Program.java
@@ -0,0 +1,49 @@
+package spring.di;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+import spring.di.ui.ExamConsole;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/* 스프링에게 지시하는 방법으로 코드를 변경
+		Exam exam=new NewlecExam();
+		Exam exam=new NewlecExam(10,10,10,10);
+		ExamConsole console=new GridExamConsole(); 
+		
+		console.setExam(exam);
+		*/
+		
+		//지시사항 알리기
+		ApplicationContext context=
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam=context.getBean(Exam.class);
+		System.out.println(exam.toString());
+		
+		//bean에 console이라는 이름의 지시서를 꺼내달라 
+		ExamConsole console=(ExamConsole)context.getBean("console");
+		//ExamConsole console=context.getBean(ExamConsole.class);
+		console.print();
+		
+		//Exam을 참조하기 위한 컬렉션 ArrayList
+		//List<Exam> exams=new ArrayList<>();
+		List<Exam> exams=(List<Exam>) context.getBean("exams");
+		exams.add(new NewlecExam(1,1,1,1));
+		
+		
+		for(Exam e:exams) {
+			System.out.println(e);
+		}
+
+	}
+
+}
